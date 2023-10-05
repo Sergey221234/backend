dc33881694c0c774513c5299b467bd5bc4691422
Sergey221234 <s.zaprudskyi@evrius.com>
2023-10-05 17:34:28 +0300

    setHeader

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ const router = require('./routes/index')
 const app = express()
 app.use(express.json())
 
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-XSS-Protection', '1; mode=block')
+  next()
+})
+
 app.use(
   cors({
     origin: 'https://admin-panel-chi-azure.vercel.app',
