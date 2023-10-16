916c97849a351603d47602594c857ca3fe08ac94
Sergey221234 <s.zaprudskyi@evrius.com>
2023-10-16 10:28:47 +0300

    sdsd

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ app.use(express.json())
 
 app.use(
   cors({
-    origin: 'https://admin-panel-chi-azure.vercel.app',
+    origin: 'https://app.n2stools.com',
     credentials: true,
   })
 )
-// const root = path.join(__dirname, 'build')
-// app.use(express.static(root))
+const root = path.join(__dirname, 'build')
+app.use(express.static(root))
 mongoose
   .connect(
     'mongodb+srv://szaprudskyi:g03n7bjxmu@cluster0.m91emua.mongodb.net/?retryWrites=true&w=majority',
@@ -56,9 +56,9 @@ app.use(
 )
 
 app.use(router)
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 const PORT = process.env.PORT || 4001
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`)
